7f461586ab27187a44aced55b394277fbab55911
Arunachalam_RM <Arunachalam_RM@outlook.com>
2025-05-03 14:05:53 +0530

    Add project files.

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace ClassLibrary
+{
+    public class Employee
+    {
+        //EmpID, EmpName, SalaryPerHour, NoOfWorkingHours and NetSalary. 
+        public int empId;
+        public string empName;
+        public double salaryPerHour;
+        public int noOfWorkingHours;
+        public double netSalary;
+        public static string OrganizationName;
+        public const string TypeOfEmployee = "Contract Based";
+        public readonly string DepartmentName;
+
+        public Employee()
+        {
+            DepartmentName = "Finance Department";
+        }
+
+        public double CalculateNetSalary()
+        {
+            return salaryPerHour*noOfWorkingHours;
+        }
+    }
+}
---
+++ WorkingWithMultipleObjects/Program.cs
@@ -0,0 +1,33 @@
+﻿using ClassLibrary;
+
+namespace Assignment
+{
+    class Sample
+    {
+        public static void Main(string[] args)
+        {
+            int i = 0;
+            string userchoice = "no";
+            do
+            {
+                System.Console.WriteLine("Employee " + ++i + " details");
+                Employee emp = new Employee();
+                System.Console.Write("\nEnter EmpId:");
+                emp.empId=int.Parse(System.Console.ReadLine());
+                System.Console.Write("\nEnter Emp Name:");
+                emp.empName = System.Console.ReadLine();
+                System.Console.Write("\nEnter Emp No of  working hours:");
+                emp.noOfWorkingHours = int.Parse(System.Console.ReadLine());
+                System.Console.Write("\nEnter Emp salary per hour:");
+                emp.salaryPerHour = System.Convert.ToDouble(System.Console.ReadLine());
+                Employee.OrganizationName = "Harsha Inc.";
+                System.Console.WriteLine("\nEmp Organization Name:" + Employee.OrganizationName);
+                System.Console.WriteLine("\nEmp Type:" + Employee.TypeOfEmployee);
+                System.Console.WriteLine("\nEmp Department:" + emp.DepartmentName);
+                System.Console.Write("\nCalculate Employee Net salary:"+emp.CalculateNetSalary());
+                System.Console.Write("\nDo you want to continue to next employee-Yes/No?");
+                userchoice =System.Console.ReadLine().ToLower();
+            } while (userchoice!="no" &&userchoice!="n" );
+        }
+    }
+}
